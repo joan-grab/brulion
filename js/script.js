e6ec7799ee4bd7fff963c29dd7554f874e806c94
joan-grab <joan.grab@gmail.com>
2020-10-04 20:49:30 +0200

    project-view-fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 document.getElementById('gallery').addEventListener('click', event => {
     console.log(event.target.className);
-    if (event.target.className === 'hover-content') { 
-         // document.getElementById('home').style.z-index='0'; 
+    if (event.target.className === 'hover-content' || 
+        event.target.className === 'title' ||
+        event.target.className === 'fas')  { 
+        window.scrollTo(0, 0);
+        document.getElementById('home').style.position='fixed';
         document.getElementById('project-view').style.display='block';
         console.log(event.target.dataset.id);
     }
@@ -10,7 +13,7 @@ document.getElementById('gallery').addEventListener('click', event => {
 document.getElementById('close').addEventListener('click', event => {
     console.log(event.target.className);
     if (event.target.className === 'fa') { 
-         // document.getElementById('home').style.z-index='0'; 
+        document.getElementById('home').style.position='static';
         document.getElementById('project-view').style.display='none';
     }
 });
