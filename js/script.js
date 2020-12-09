548da7586150d88be5bb4345ed3d03faee3f15f3
joan-grab <joan.grab@gmail.com>
2020-12-09 12:54:14 +0100

    closing project-view

--- js/script.js
+++ js/script.js
@@ -2,26 +2,26 @@ const openGallery = document.getElementById('gallery');
 
 if (openGallery !== null) {
     openGallery.addEventListener('click', event => {
-        console.log(event.target.className);
         if (event.target.className === 'hover-content' || 
             event.target.parentElement.className === 'title' ||
             event.target.className === 'fas')  { 
             window.scrollTo(0, 0);
             document.getElementById('home').style.position='fixed';
-            document.getElementById('project-view').style.display='block';
+            document.getElementById('project-view-home').style.display='block';
             console.log(event.target.dataset.id);
         }
     });
 }
 
-const closeGallery = document.getElementById('close');
+const closeGallery = document.getElementById('project-view-home');
 
 if (closeGallery !== null) {
     closeGallery.addEventListener('click', event => {
-        console.log(event.target.className);
-        if (event.target.id === 'home-close') { 
+        console.log(event.target.id);
+        if (event.target.id === 'project-view-home' ||
+            event.target.id === 'home-close' ) { 
             document.getElementById('home').style.position='static';
-            document.getElementById('project-view').style.display='none';
+            document.getElementById('project-view-home').style.display='none';
         }
     });
 }
@@ -30,27 +30,28 @@ const openPortfolio = document.getElementById('portfolio-content');
 
 if (openPortfolio !== null) {
     openPortfolio.addEventListener('click', event => {
-        console.log(event.target.className);
         if (event.target.className === 'project' || 
             event.target.className === 'project-image' ||
             event.target.parentElement.className === 'project-image' ||
             event.target.className === 'project-title' ||
             event.target.parentElement.className === 'project-title')  { 
             document.getElementById('portfolio-page').style.position='fixed';
-            document.getElementById('project-view').style.display='block';
+            document.getElementById('project-view-portfolio').style.display='block';
             console.log(event.target.dataset.id);
         }
     });
 }
 
-const closePortfolio = document.getElementById('close');
+const closePortfolio = document.getElementById('project-view-portfolio');
 
 if (closePortfolio !== null) {
-    document.getElementById('close').addEventListener('click', event => {
+    closePortfolio.addEventListener('click', event => {
+        console.log(event.target.id);
         console.log(event.target.className);
-        if (event.target.id === 'port-close') { 
+        if (event.target.id === 'project-view-portfolio' ||
+            event.target.id === 'port-close' ) { 
             document.getElementById('portfolio-page').style.position='static';
-            document.getElementById('project-view').style.display='none';
+            document.getElementById('project-view-portfolio').style.display='none';
         }
     });
 }
